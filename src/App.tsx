174d3f1900bf0a1956db6fce54bd368713075969
Rafal <rgrajcar@gmail.com>
2022-06-15 23:31:00 +0200

    added background to main view prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Header} from "./components/Header/Header";
 
+import background from './background.jpg';
+
+import classes from './App.module.css';
+
 export const App = () => {
 
   return (
-    <div>
+    <div className={classes.wrapper}>
       <Header />
+      <div style={{height: '100%', backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'top', backgroundRepeat: 'no-repeat'}}/>
     </div>
   );
 }
