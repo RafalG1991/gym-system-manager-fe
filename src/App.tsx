541567855488a932b720db3e3d62cb68f5e7ba69
Rafal <rgrajcar@gmail.com>
2022-06-16 22:30:47 +0200

    fixed the background for unauthorized app login form

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import {Header} from "./components/Header/Header";
 import background from './background.jpg';
 
 import classes from './App.module.css';
+import {AuthForm} from "./components/AuthForm/AuthForm";
 
 export const App = () => {
 
   return (
     <div className={classes.wrapper}>
       <Header />
-      <div style={{height: '100%', backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'top', backgroundRepeat: 'no-repeat'}}/>
+      <div style={{height: '100%', backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'top', backgroundRepeat: 'no-repeat', filter: 'blur(4px)', zIndex: -1}}/>
+        <AuthForm/>
     </div>
   );
 }
