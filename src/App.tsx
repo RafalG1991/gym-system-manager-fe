3f4511d47a3dfa336b33eb76b2156ff1c03722b7
Rafal <rgrajcar@gmail.com>
2022-06-22 07:55:31 +0200

    added layout for authorized view outside App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import React, {useContext} from 'react';
 import {Header} from "./components/Header/Header";
-
-import background from './background.jpg';
-
 import classes from './App.module.css';
-import {AuthForm} from "./components/AuthForm/AuthForm";
 import {AuthContext} from "./providers/AuthProvider";
-import {Navigate, Route, Routes} from "react-router-dom";
 import {UnauthorizedApp} from "./components/Layouts/UnauthorizedApp";
+import {AuthorizedApp} from "./components/Layouts/AuthorizedApp";
 
 
 
-const AuthorizedApp = () => {
-  return (
-    <h1>This is authorized app</h1>
-  )
-}
+
 
 export const App = () => {
   const {user} = useContext(AuthContext);
@@ -23,7 +15,6 @@ export const App = () => {
   return (
     <div className={classes.wrapper}>
       <Header />
-
       {
         user ? <AuthorizedApp /> : <UnauthorizedApp />
       }
