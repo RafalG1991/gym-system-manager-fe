6c26aa937d0f1f502114f87c6373e725e53ec1af
Rafal <rgrajcar@gmail.com>
2022-07-09 17:56:52 +0200

    added EventInfo component with day of the week and 12h time calculations

--- src/components/SingleClass/SingleClass.tsx
+++ src/components/SingleClass/SingleClass.tsx
@@ -5,6 +5,7 @@ import {useError} from "../../hooks/use-error";
 import {Loader} from "../utilities/Loader/Loader";
 import { ClassEntity } from 'types';
 import {Link} from "react-router-dom";
+import {EventInfo} from "../EventInfo/EventInfo";
 
 interface Props {
   eventId: string;
@@ -49,40 +50,9 @@ export const SingleClass = ({eventId}: Props) => {
     return <p>The event with this ID doesnt exist</p>
   }
 
-  const convertTime = (time: string) => {
-    let [hours, minutes] = time.split(':');
-    const amOrPm = Number(hours)>=12 ? 'pm' : 'am';
-    hours = Number(hours)%12+'' || '12';
-    return (hours==='0' && amOrPm==='pm' ? '12' : hours)+':'+minutes+' '+amOrPm;
-  }
-
-  const getWeekDay = (dayNum: number) => {
-    switch (dayNum) {
-      case 0:
-        return 'SUNDAY';
-      case 1:
-        return 'MONDAY';
-      case 2:
-        return 'TUESDAY';
-      case 3:
-        return 'WEDNESDAY';
-      case 4:
-        return 'THURSDAY';
-      case 5:
-        return 'FRIDAY';
-      case 6:
-        return 'SATURDAY';
-    }
-  }
-
   return <div className={classes.wrapper}>
     <h1>{event.name}</h1>
-    <div>
-      <p>Every <span className={classes.highlightedText}>{getWeekDay(event.day)}</span></p>
-      <p>From <span className={classes.highlightedText}>{convertTime(event.starts)}</span> to <span className={classes.highlightedText}>{convertTime(event.ends)}</span></p>
-      <h3>Description</h3>
-      <p className={classes.description}>{event.description}</p>
-    </div>
+    <EventInfo event={event}/>
     <Link className={classes.backButton} to='/classes'>Back</Link>
   </div>
 }
